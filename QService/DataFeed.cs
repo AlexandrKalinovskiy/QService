201939f0369402ab10d6d07790952efbd279d680
Александр <alexandr.kalinovskiy@gmail.com>
2016-03-04 16:51:13 +0000

    FIX

--- QService/DataFeed.cs
+++ QService/DataFeed.cs
@@ -67,7 +67,6 @@ namespace QService
                 connector.UnRegisterSecurity(security);
             }
             connector.Disconnect();
-            connector.Dispose();
         }
 
         ~DataFeed()
