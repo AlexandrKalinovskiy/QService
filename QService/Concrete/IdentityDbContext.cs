9b199f5e8fa10dfd7d140de1b57d8a6ed77835b8
Александр <alexandr.kalinovskiy@gmail.com>
2016-03-22 22:56:03 +0600

    Commit

---
+++ QService/Concrete/IdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using QService.Admin;
+
+namespace QService.Concrete
+{
+    public partial class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext() : base("IdentityDbContext") { }
+
+        public static IdentityDbContext Create()
+        {
+            return new IdentityDbContext();
+        }
+    }
+}
